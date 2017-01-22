8c36a89190cdcb1c21d17ddede6bb681490f2b74
Jonathan Stassen <Jonathan.Stassen@sproutsocial.com>
2017-01-21 19:11:49 -0600

    popout js

--- app/scripts.babel/popup.js
+++ app/scripts.babel/popup.js
@@ -1,8 +1,25 @@
 'use strict';
 
 console.log('Fooda fooda! Popup');
-	chrome.alarms.getAll( function(alarms) {
-		console.log('alarms after: ', alarms);
-		debugger;
-		$('.alarms').append('<div></div>');
+chrome.alarms.getAll(function (alarms) {
+	$('.alarms').append('<p>Nothing order for these days yet..</p>');
+	alarms.forEach(function(alarm) {
+		var date = moment(alarm.name, 'YYYY-MM-DD');
+		if (date)
+		var text = date.format('ll');
+		$('.alarms').append('<li><a href=https://app.fooda.com/my?date='+alarm.name+ '>'+text+'</a></li>');
 	});
+});
+
+document.addEventListener('DOMSubtreeModified', function () {
+	var links = document.getElementsByTagName("a");
+	for (var i = 0; i < links.length; i++) {
+		(function () {
+			var ln = links[i];
+			var location = ln.href;
+			ln.onclick = function () {
+				chrome.tabs.create({active: true, url: location});
+			};
+		})();
+	}
+});
